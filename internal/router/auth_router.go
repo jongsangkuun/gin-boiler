6effd46be87624b1e93f0f4df828733b3ee937c1
jongsangkuun <43625848+jongsangkuun@users.noreply.github.com>
2025-09-16 11:16:58 +0900

    feat: 로그아웃 라우트 제거 및 인증 라우트 정리

--- internal/router/auth_router.go
+++ internal/router/auth_router.go
@@ -10,7 +10,6 @@ func SetupAuthRoutes(api *gin.RouterGroup) (a *gin.RouterGroup) {
 	authRoutes := api.Group("/auth")
 	{
 		authRoutes.GET("/login", service.LoginService)
-		authRoutes.GET("/logout", service.LogoutService)
 	}
 	return authRoutes
 }
